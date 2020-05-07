2362e460cf72236c97fd3cb32b575dc04836939f
Alexey Tikhvinskiy <lexis.tikh@gmail.com>
2020-05-07 23:57:07 +0300

    Trying to create P2SH transaction

--- TestCluster.py
+++ TestCluster.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
+from time import sleep
+
 import bitcoin
 
 import util
-from test_framework.address import program_to_witness, key_to_p2pkh, byte_to_base58
+from test_framework.address import program_to_witness, key_to_p2pkh, key_to_p2sh_p2wpkh, script_to_p2sh, byte_to_base58
 from test_framework.key import generate_key_pair, ECKey
-from test_framework.messages import COutPoint, CTransaction, CTxIn, CTxOut, CTxInWitness
+from test_framework.messages import COutPoint, CTransaction, CTxIn, CTxOut, CTxInWitness, FromHex
 import test_framework
 from test_framework.script import CScript, OP_IF, OP_ELSE, OP_ENDIF, OP_HASH160, OP_EQUAL, OP_CHECKSIG, \
     SegwitV0SignatureHash, get_p2pkh_script, SIGHASH_ALL, CScriptOp
 from test_framework.segwit_addr import bech32_decode
 from util import TestWrapper
+import base58
+import bitcoin
 
 test = TestWrapper()
 # Start TestNodes
@@ -24,7 +28,7 @@ node1 = test.nodes[0]  # coin sender
 node2 = test.nodes[1]  # coin receiver
 node3 = test.nodes[2]  # mainer
 
-node1_addr = node1.getnewaddress(address_type="bech32")
+node1_addr = node1.getnewaddress(address_type="legacy")
 node2_addr = node2.getnewaddress(address_type="bech32")
 node3_addr = node3.getnewaddress(address_type="bech32")
 
@@ -32,106 +36,125 @@ node1.generatetoaddress(250, node1_addr)
 node2.generatetoaddress(250, node2_addr)
 # node3.generate(2)
 
+node1_priv = node1.dumpprivkey(node1_addr)
+node1_key_bytes = base58.b58decode_check(node1_priv)
+print("Node1 key: {}".format(node1_key_bytes.hex()))
+
 balance1 = node1.getbalance()
 print('Balance node 1:', balance1)
 
 balance2 = node2.getbalance()
 print('Balance node 2:', balance2)
 
-priv = ECKey()
-priv.generate()
+# Spending key
+spending_key = ECKey()
+spending_key.generate(compressed=True)
 
-#init_key = byte_to_base58(priv.get_bytes(), 0xef)
-init_addr = key_to_p2pkh(priv.get_pubkey().get_bytes())
+spending_wif = bitcoin.encode_privkey(spending_key.get_bytes(), "wif_compressed", 111)
+# print("WIF: {}".format(wif))
 
-print("Initial WIP key: {}".format(init_key))
-print("Initial addr: {}".format(init_addr))
+# spending_wif = byte_to_base58(spending_key.get_bytes()+b'0x01', 111)
+spending_addr = key_to_p2pkh(spending_key.get_pubkey().get_bytes())
 
-#node1.importprivkey(init_key, "Test key", False)
-node1.importaddress(init_addr, "Test address", False)
+print("Spending WIF: {}".format(spending_wif))
+print("Spending addr: {}".format(spending_addr))
 
-tx = node1.sendtoaddress(init_addr, 20, "Initial transaction")
-print("Initial transaction id: {}".format(tx))
+node1.importprivkey(spending_wif, "Spending key", False)
+node1.importaddress(spending_addr, "Spending address", False)
 
-node1.generatetoaddress(10, node1_addr)
+# Receiving key
+receiving_key = ECKey()
+receiving_key.generate(compressed=True)
 
-# test_transactions = node1.listtransactions("Initial transaction")
-# print(test_transactions)
+receiving_addr = key_to_p2sh_p2wpkh(receiving_key.get_pubkey().get_bytes())
+node2.importaddress(receiving_addr, "Receiving address", False)
 
-#print(node1.listunspent(addresses=[init_addr]))
+# Send coins to spending address
+unspent_txid = node1.sendtoaddress(spending_addr, 20, "Initial transaction")
+print("Initial transaction id: {}".format(unspent_txid))
 
-init_balance = node1.getreceivedbyaddress(init_addr)
+node1.generatetoaddress(1, node1_addr)
+
+init_balance = node1.getreceivedbyaddress(spending_addr)
 print('Initial balance:', init_balance)
 
-spending_tx = CTransaction()
-
-spending_tx.nVersion = 1
-spending_tx.nLockTime = 0
-
-unspent_txid = node1.listunspent(1)[-1]["txid"]
-outpoint = COutPoint(int(unspent_txid, 16), 0)
-spending_tx_in = CTxIn(outpoint)
-spending_tx.vin = [spending_tx_in]
-
-channel_addr_1 = node1_addr
-channel_addr_2 = node2_addr
+# Create channel
 
 secret = b'secret'
 
-# Transaction script from https://medium.com/softblocks/lightning-network-in-depth-part-2-htlc-and-payment-routing-db46aea445a8
-# but without timeout condition for simplicity
+channel_script = CScript([CScriptOp(OP_HASH160), secret, CScriptOp(OP_EQUAL),
+                          CScriptOp(OP_IF),
+                          spending_key.get_pubkey().get_bytes(),
+                          CScriptOp(OP_ELSE),
+                          receiving_key.get_pubkey().get_bytes(),
+                          CScriptOp(OP_ENDIF),
+                          CScriptOp(OP_CHECKSIG)])
 
-node1_info = node1.getaddressinfo(channel_addr_1)
-node2_info = node2.getaddressinfo(channel_addr_2)
+script_addr = script_to_p2sh(channel_script)
 
-# print(node1_info)
-# print(node2_info)
+print("Channel script addr: {}".format(script_addr))
 
-script = CScript([CScriptOp(OP_HASH160), secret, CScriptOp(OP_EQUAL),
-                  CScriptOp(OP_IF),
-                  bytes.fromhex(node1_info['pubkey']), CScriptOp(OP_CHECKSIG),
-                  CScriptOp(OP_ELSE),
-                  bytes.fromhex(node2_info['pubkey']), CScriptOp(OP_CHECKSIG),
-                  CScriptOp(OP_ENDIF)])
+channel_tx = node1.createrawtransaction(inputs=[{"txid": unspent_txid, "vout": 0}], outputs=[{script_addr: 15}, {spending_addr: 4.99}])
+# sign_res = node1.signrawtransactionwithkey(channel_tx, [spending_wif])
+sign_res = node1.signrawtransactionwithwallet(channel_tx)
 
-sighash = SegwitV0SignatureHash(script=script,
-                                txTo=spending_tx,
-                                inIdx=0,
-                                hashtype=SIGHASH_ALL,
-                                amount=100_000_000)
+print(sign_res)
 
-k1 = node1.dumpprivkey(node1_info['address'])
-print(k1)
+channel = CTransaction()
+FromHex(channel, channel_tx)
+print(channel)
 
-# k2 = node2.dumpprivkey(node2_info['address'])
-# print(k2)
+res = node1.sendrawtransaction(channel_tx)
+print(res)
 
-key = bitcoin.decode_privkey(k1)
-print(key)
+# channel_sig = spending_key.sign_ecdsa(  channel_tx)
 
-priv1 = ECKey()
-priv1.set(key)
-
-sig = priv1.sign_ecdsa(sighash) + chr(SIGHASH_ALL).encode('latin-1')
-
-print("Signature: {}\n".format(sig.hex()))
-
-# Add a witness to the transaction. For a P2WPKH, the witness field is the signature and pubkey
-spending_tx.wit.vtxinwit.append(CTxInWitness([sig.hex(), channel_addr_1]))
-
-# print(spending_tx)
-
-# Serialize signed transaction for broadcast
-spending_tx_str = spending_tx.serialize()
-
-# Test mempool acceptance
-assert node1.testmempoolaccept(rawtxs=[spending_tx_str], maxfeerate=0)[0]['allowed']
-
-# node1.
+# sig_res = node1.signrawtransactionwithkey(channel_tx, )
 
 
-# addr1 = node1.get_deterministic_priv_key().address
-# program = hash160(addr1)
+# channel_txid = node1.sendtoaddress(script_addr, 15)
+
+node1.generatetoaddress(1, node1_addr)
+
+init_balance = node1.getreceivedbyaddress(spending_addr)
+print('Balance after channel opening:', init_balance)
+
+# spending_tx = CTransaction()
+#
+# spending_tx.nVersion = 1
+# spending_tx.nLockTime = 0
+#
+# outpoint = COutPoint(int(unspent_txid, 16), 0)
+# spending_tx_in = CTxIn(outpoint)
+# spending_tx.vin = [spending_tx_in]
+#
+# # Transaction script from https://medium.com/softblocks/lightning-network-in-depth-part-2-htlc-and-payment-routing-db46aea445a8
+# # but without timeout condition for simplicity
+#
+#
+# sighash = SegwitV0SignatureHash(script=script,
+#                                 txTo=spending_tx,
+#                                 inIdx=0,
+#                                 hashtype=SIGHASH_ALL,
+#                                 amount=100_000_000)
+#
+# sig = spending_key.sign_ecdsa(sighash) + chr(SIGHASH_ALL).encode('latin-1')
+#
+# address = program_to_witness(version, program)
+# print("bech32 address: {}".format(address))
+#
+# print("Signature: {}\n".format(sig.hex()))
+#
+# # Add a witness to the transaction. For a P2WPKH, the witness field is the signature and pubkey
+# spending_tx.wit.vtxinwit.append(CTxInWitness([sig.hex(), channel_addr_1]))
+#
+# # print(spending_tx)
+#
+# # Serialize signed transaction for broadcast
+# spending_tx_str = spending_tx.serialize()
+#
+# # Test mempool acceptance
+# assert node1.testmempoolaccept(rawtxs=[spending_tx_str], maxfeerate=0)[0]['allowed']
 
 
 test.shutdown()
