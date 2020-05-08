048fa8ad0a7d6ccaa0a0670f1a0ab2aa5b4cf6dd
Alexey Tikhvinskiy <lexis.tikh@gmail.com>
2020-05-08 20:55:25 +0300

    Create channel transaction (with bugs so far)

--- TestCluster.py
+++ TestCluster.py
@@ -8,8 +8,8 @@ from test_framework.address import program_to_witness, key_to_p2pkh, key_to_p2sh
 from test_framework.key import generate_key_pair, ECKey
 from test_framework.messages import COutPoint, CTransaction, CTxIn, CTxOut, CTxInWitness, FromHex
 import test_framework
-from test_framework.script import CScript, OP_IF, OP_ELSE, OP_ENDIF, OP_HASH160, OP_EQUAL, OP_CHECKSIG, \
-    SegwitV0SignatureHash, get_p2pkh_script, SIGHASH_ALL, CScriptOp
+from test_framework.script import CScript, CScriptOp, OP_IF, OP_ELSE, OP_ENDIF, OP_HASH160, OP_EQUAL, OP_CHECKSIG, \
+    SegwitV0SignatureHash, get_p2pkh_script, hash160, SIGHASH_ALL
 from test_framework.segwit_addr import bech32_decode
 from util import TestWrapper
 import base58
@@ -47,46 +47,53 @@ balance2 = node2.getbalance()
 print('Balance node 2:', balance2)
 
 # Spending key
-spending_key = ECKey()
-spending_key.generate(compressed=True)
+key0 = ECKey()
+key0.generate(compressed=True)
 
-spending_wif = bitcoin.encode_privkey(spending_key.get_bytes(), "wif_compressed", 111)
+# spending_wif = bitcoin.encode_privkey(spending_key.get_bytes(), "wif_compressed", 111)
 # print("WIF: {}".format(wif))
 
-# spending_wif = byte_to_base58(spending_key.get_bytes()+b'0x01', 111)
-spending_addr = key_to_p2pkh(spending_key.get_pubkey().get_bytes())
+# spending_wif = byte_to_base58(key0.get_bytes()+b'0x01', 111)
+addr0 = key_to_p2pkh(key0.get_pubkey().get_bytes())
 
-print("Spending WIF: {}".format(spending_wif))
-print("Spending addr: {}".format(spending_addr))
+# print("Spending WIF: {}".format(spending_wif))
+print("addr0: {}".format(addr0))
 
-node1.importprivkey(spending_wif, "Spending key", False)
-node1.importaddress(spending_addr, "Spending address", False)
+# node1.importprivkey(spending_wif, "key0", False)
+node1.importaddress(addr0, "addr0", False)
 
 # Receiving key
-receiving_key = ECKey()
-receiving_key.generate(compressed=True)
+key1 = ECKey()
+key1.generate(compressed=True)
 
-receiving_addr = key_to_p2sh_p2wpkh(receiving_key.get_pubkey().get_bytes())
-node2.importaddress(receiving_addr, "Receiving address", False)
+addr1 = key_to_p2sh_p2wpkh(key1.get_pubkey().get_bytes())
+# node2.importaddress(addr1, "addr1", False)
 
 # Send coins to spending address
-unspent_txid = node1.sendtoaddress(spending_addr, 20, "Initial transaction")
-print("Initial transaction id: {}".format(unspent_txid))
+tx0_id = node1.sendtoaddress(addr0, 20, "Initial transaction")
+print("Initial transaction id: {}".format(tx0_id))
 
-node1.generatetoaddress(1, node1_addr)
+node1.generatetoaddress(2, node1_addr)
 
-init_balance = node1.getreceivedbyaddress(spending_addr)
+init_balance = node1.getreceivedbyaddress(addr0)
 print('Initial balance:', init_balance)
 
-# Create channel
+tx0_data = node1.gettransaction(tx0_id)
+tx0 = FromHex(CTransaction(), tx0_data["hex"])
+print(tx0)
+
+
+# Transaction script from https://medium.com/softblocks/lightning-network-in-depth-part-2-htlc-and-payment-routing-db46aea445a8
+# but without timeout condition for simplicity
 
 secret = b'secret'
+secret_hash = hash160(secret)
 
-channel_script = CScript([CScriptOp(OP_HASH160), secret, CScriptOp(OP_EQUAL),
+channel_script = CScript([CScriptOp(OP_HASH160), secret_hash, CScriptOp(OP_EQUAL),
                           CScriptOp(OP_IF),
-                          spending_key.get_pubkey().get_bytes(),
+                          key0.get_pubkey().get_bytes(),
                           CScriptOp(OP_ELSE),
-                          receiving_key.get_pubkey().get_bytes(),
+                          key1.get_pubkey().get_bytes(),
                           CScriptOp(OP_ENDIF),
                           CScriptOp(OP_CHECKSIG)])
 
@@ -94,46 +101,31 @@ script_addr = script_to_p2sh(channel_script)
 
 print("Channel script addr: {}".format(script_addr))
 
-channel_tx = node1.createrawtransaction(inputs=[{"txid": unspent_txid, "vout": 0}], outputs=[{script_addr: 15}, {spending_addr: 4.99}])
-# sign_res = node1.signrawtransactionwithkey(channel_tx, [spending_wif])
-sign_res = node1.signrawtransactionwithwallet(channel_tx)
-
-print(sign_res)
-
 channel = CTransaction()
-FromHex(channel, channel_tx)
-print(channel)
+channel.nVersion = 1
+channel.nLockTime = 0
+
+outpoint = COutPoint(int(tx0_id, 16), 0)
+channel_in = CTxIn(outpoint)
+channel.vin = [channel_in]
+
+channel_out = CTxOut(499_990_000, CScript([OP_HASH160, hash160(channel_script), OP_EQUAL]))
+channel.vout = [channel_out]
+
+channel_tx = channel.serialize_without_witness()
+channel_vin_sig = key0.sign_ecdsa(channel_tx)
+channel.vin[0].scriptSig = CScript([channel_vin_sig, key0.get_pubkey().get_bytes()])
+
+channel_tx = channel.serialize().hex()
+
+print(channel_tx)
 
 res = node1.sendrawtransaction(channel_tx)
+# res = node1.testmempoolaccept(rawtxs=[channel_tx], maxfeerate=0)
 print(res)
 
-# channel_sig = spending_key.sign_ecdsa(  channel_tx)
-
-# sig_res = node1.signrawtransactionwithkey(channel_tx, )
-
-
-# channel_txid = node1.sendtoaddress(script_addr, 15)
-
-node1.generatetoaddress(1, node1_addr)
-
-init_balance = node1.getreceivedbyaddress(spending_addr)
-print('Balance after channel opening:', init_balance)
-
-# spending_tx = CTransaction()
-#
-# spending_tx.nVersion = 1
-# spending_tx.nLockTime = 0
-#
-# outpoint = COutPoint(int(unspent_txid, 16), 0)
-# spending_tx_in = CTxIn(outpoint)
-# spending_tx.vin = [spending_tx_in]
-#
-# # Transaction script from https://medium.com/softblocks/lightning-network-in-depth-part-2-htlc-and-payment-routing-db46aea445a8
-# # but without timeout condition for simplicity
-#
-#
-# sighash = SegwitV0SignatureHash(script=script,
-#                                 txTo=spending_tx,
+# sighash = SegwitV0SignatureHash(script=channel_script,
+#                                 txTo=spescriptnding_tx,
 #                                 inIdx=0,
 #                                 hashtype=SIGHASH_ALL,
 #                                 amount=100_000_000)
